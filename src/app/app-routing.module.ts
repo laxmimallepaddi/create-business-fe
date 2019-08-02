fc92886695bc5726c8c074dd344655cee55bafb5
Smriti <smritichangulani@gmail.com>
2019-08-03 00:54:47 +0530

    HomePage-Login-Search Bar and others

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { FindBusinessComponent } from './find-business/find-business.component';
-
+import { RegistercustomerRegisterComponent } from './register/registercustomer-register/registercustomer-register.component';
+import { RegisterbusinessRegisterComponent } from './register/registerbusiness-register/registerbusiness-register.component';
 
 const routes: Routes = [
   { path:'', component:HomeComponent },
   { path:'login', component:LoginComponent },
   { path:'register', component:RegisterComponent },
-  { path:'findbusiness', component:FindBusinessComponent }
+  { path:'findbusiness', component:FindBusinessComponent },
+  { path:'register/business', component:RegisterbusinessRegisterComponent },
+  { path:'register/customer', component:RegistercustomerRegisterComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { FindBusinessComponent } from './find-business/find-business.component';
-
+import { RegistercustomerRegisterComponent } from './register/registercustomer-register/registercustomer-register.component';
+import { RegisterbusinessRegisterComponent } from './register/registerbusiness-register/registerbusiness-register.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,9 @@ import { FindBusinessComponent } from './find-business/find-business.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    FindBusinessComponent
+    FindBusinessComponent,
+    RegistercustomerRegisterComponent,
+    RegisterbusinessRegisterComponent
   ],
   imports: [
     BrowserModule,
